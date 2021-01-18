4e55388e8ded77c381c117ef51414204d4b8de5f
akaaariii <caaari23@gmail.com>
2021-01-18 03:03:29 -0800

    update using mongoose

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
-// const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
-const database = require('./util/database');
 
 //Model
-const members = require('./model/members.model');
+const Member = require('./model/members.model');
 //Routes
 const memberRoute = require('./routes/members.routes');
 
@@ -14,20 +14,33 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(methodOverride('_method'));
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 //home route
-app.get('/', (req, res) => {
-    res.render('index', { title: "Members List", members });
+app.get('/', async(req, res) => {
+  const members = await Member.find({});
+  console.log({ members });
+
+  res.render('index', { title: "Members List", members });
 });
 
 //members routes
 app.use('/api/members', memberRoute);
 
 
-database.mongoConnect(() => {
-    app.listen(3000, () => console.log(`Server is running on port 3000`));
-})
-
-
+mongoose
+  .connect("mongodb://127.0.0.1:27017/member_manager", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log("Connected to Database!");
+    app.listen(3000, () => console.log(`Server has started on PORT 3000`));
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- model/members.model.js
+++ model/members.model.js
@@ -1,117 +1,20 @@
-// const { v4: uuid } = require("uuid");
-const mongodb = require("mongodb");
-const database = require("../util/database");
+const mongoose = require('mongoose');
 
-let members = [
-  {
-    id: "1",
-    name: "JC",
-    email: "jc@mail.com",
-    status: "active",
+const memberSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
   },
-  {
-    id: "2",
-    name: "ABC",
-    email: "abc@mail.com",
-    status: "active",
+  email: {
+    type: String,
+    required: true
   },
-  {
-    id: "3",
-    name: "Mart",
-    email: "mart@mail.com",
-    status: "active",
+  status: {
+    type: String,
+    required: true
   },
-];
+});
 
-class Member {
-  constructor(name, email) {
-    // this.id = uuid();
-    this.name = name;
-    this.email = email;
-    this.status = "active";
-  }
-
-  saveMember(callback) {
-    let db = database.getDB();
-    db.collection("members")
-      .insertOne(this)
-      .then((result) => {
-        console.log("Insert: ", result);
-      })
-      .catch((err) => console.error(err));
-    // members.push(this);
-    callback(this);
-  }
-
-  static getAllMembers(callback) {
-    let db = database.getDB();
-    db.collection("members")
-      .find({})
-      .toArray((err, result) => {
-        if (err) throw err;
-        callback(result);
-      });
-    // callback(members);
-  }
-
-  static getOneMember(id, callback) {
-    let db = database.getDB();
-    db.collection("members")
-      .findOne({ _id: new mongodb.ObjectID(id) })
-      .then((result) => {
-        callback(result);
-      })
-      .catch(err => {
-        callback(err);
-      })
-    // const found = members.some((member) => member.id === id);
-
-    // if (found) {
-    //   callback(members.filter((member) => member.id === id));
-    // } else {
-    //   callback([]);
-    // }
-  }
-
-  static updateMember(id, body, callback) {
-    let db = database.getDB();
-    db.collection("members").updateOne(
-      { _id: new mongodb.ObjectID(id) },
-      { $set: { ...body } },
-      (err, response) => {
-        callback(!!response.modifiedCount);
-      }
-    );
-    // const found = members.some((member) => member.id === id);
-
-    // if (found) {
-    //     const memberToUpdate = members.find(member => member.id === id);
-    //     const updatedMember = {
-    //         ...memberToUpdate,
-    //         ...body
-    //     }
-    //     callback([updatedMember]);
-    // } else {
-    //     callback([]);
-    // }
-  }
-
-  static deleteMember(id, callback) {
-    let db = database.getDB();
-    db.collection("members").deleteOne(
-      { _id: new mongodb.ObjectID(id) },
-      (err, response) => {
-        callback(!!response.deletedCount);
-      }
-    );
-    // const found = members.some((member) => member.id === id);
-
-    // if (found) {
-    //     callback(members.filter(member => member.id !== id));
-    // } else {
-    //     callback([]);
-    // }
-  }
-}
+const Member = mongoose.model('Member', memberSchema);
 
 module.exports = Member;
